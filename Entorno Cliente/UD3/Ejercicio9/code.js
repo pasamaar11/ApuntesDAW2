247cb79cb665411bb32404299792c5601ed8275e
José Javier <jpasamaracon@gmail.com>
2025-11-19 00:02:46 +0100

    Ejercicio9_Sorteo_Completado

--- Entorno Cliente/UD3/Ejercicio9/code.js
+++ Entorno Cliente/UD3/Ejercicio9/code.js
@@ -13,10 +13,22 @@ function mostrarAlumnos() {
 function sorteoAlumnos() {
     const indiceGanador = Math.floor(Math.random() * listaAlumnos.length);
     const ganador = listaAlumnos[indiceGanador];
+
+    // Mostrar ganador
     document.getElementById("ganador").textContent = "Ganador del sorteo: " + ganador;
+
+    // Eliminar ganador del array
+    listaAlumnos.splice(indiceGanador, 1);
+    if (listaAlumnos.length === 0) {
+        alert("No hay más alumnos para sortear.");
+        return;
+    }
+
+    // Actualizar la lista mostrada en pantalla
+    mostrarAlumnos();
 }
 
-
+// Con esto mostramos la lista al cargar la páginaF
 window.onload = function() {
     mostrarAlumnos();
-};
+}
