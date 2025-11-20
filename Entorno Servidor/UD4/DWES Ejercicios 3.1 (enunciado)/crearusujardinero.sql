fe0e6d8e21a08bfa30e52f0c6fd750811e6ea1cf
pasamaar11 <jpasamaracon@gmail.com>
2025-11-20 15:39:36 +0100

    EnunciadosServidor

---
+++ Entorno Servidor/UD4/DWES Ejercicios 3.1 (enunciado)/crearusujardinero.sql
@@ -0,0 +1,3 @@
+CREATE USER 'jardinero'@'localhost' IDENTIFIED BY 'jardinero';
+GRANT USAGE ON *.* TO 'jardinero'@'localhost' IDENTIFIED BY 'jardinero' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+GRANT ALL PRIVILEGES ON `jardineria`.* TO 'jardinero'@'localhost';
