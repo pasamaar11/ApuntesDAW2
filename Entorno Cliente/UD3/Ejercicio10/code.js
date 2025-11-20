5916f00f428898dd83167f6a1b6905c2a9243f0d
pasamaar11 <jpasamaracon@gmail.com>
2025-11-20 16:16:36 +0100

    Calculadora_HECHA

---
+++ Entorno Cliente/UD3/Ejercicio10/code.js
@@ -0,0 +1,41 @@
+let currentInput = "";
+let operator = "";
+let previousInput = "";
+
+function appendNumber(number) {
+  currentInput += number;
+  document.getElementById("display").value = currentInput;
+}
+
+function setOperator(op) {
+  if(currentInput === ""){
+    return "";
+  }
+  operator = op;
+  previousInput = currentInput;
+  currentInput = "";
+}
+
+function calculate() {
+  let result;
+  if (operator === "+") {
+    result = parseFloat(previousInput) + parseFloat(currentInput);
+  } else if (operator === "-") {
+    result = parseFloat(previousInput) - parseFloat(currentInput);
+  } else if (operator === "*") {
+    result = parseFloat(previousInput) * parseFloat(currentInput);
+  } else if (operator === "/") {
+    result = parseFloat(previousInput) / parseFloat(currentInput);
+  }
+
+  document.getElementById('display').value = result;
+  currentInput = result.toString();
+  operator = "";
+}
+
+function clearDisplay(){
+  currentInput = "";
+  previousInput = "";
+  operator = "";
+  document.getElementById('display').value = "";
+}
