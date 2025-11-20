07eb5243d5d29db421efbc901b3924a70466efb4
pasamaar11 <jpasamaracon@gmail.com>
2025-11-20 17:54:53 +0100

    Ejercicio6_UD4_Cliente

---
+++ Entorno Cliente/UD4/Ejercicio 6/code.js
@@ -0,0 +1,30 @@
+const inputTlf = document.getElementById("telefono");
+const validar = document.getElementById("validar");
+const mensaje = document.getElementById("mensaje");
+
+function esFijo(numero) {
+    if (numero[0] !== "8" && numero[0] !== "9") {
+        return false;
+    } else if (numero.length !== 9) {
+        return false;
+    } else if (numero[0] === "-") {
+        return false;
+    } else if (numero[1] === "0" && numero[2] === "0") {
+        return false;
+    } else {
+        return true;
+    }
+}
+function validacionMensaje(){
+    const valor =  inputTlf.value;
+
+    if(esFijo(valor)){
+        mensaje.style.color = "green";
+        mensaje.textContent = "Número de teléfono válido";
+    }else{
+        mensaje.style.color = "red";
+        mensaje.textContent = "Número de teléfono NO válido";
+    }
+}
+
+validar.addEventListener('click', validacionMensaje);
