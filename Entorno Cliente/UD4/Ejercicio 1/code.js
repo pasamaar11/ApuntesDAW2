986abefe503262d6153f7d9249ad598e790d6e55
José Javier <jpasamaracon@gmail.com>
2025-11-21 23:54:29 +0100

    Ejercicio1_UD4_DWEC_REALIZADO

--- Entorno Cliente/UD4/Ejercicio 1/code.js
+++ Entorno Cliente/UD4/Ejercicio 1/code.js
@@ -1,9 +1,21 @@
-const salarioB = document.getElementById("salarioB");
-const retencion = document.getElementById("retencion");
-const nPagas = document.getElementById("nPagas");
+const botonGenerar = document.getElementById("generar");
 
 function calculoSalarioNeto(){
-    var sbm = (salarioB / nPagas);
-    var factorRetencion = (retencion / 100);
-    var retencionMensual = (sbm * factorRetencion);
+    //Obtenemos los elementos del DOM (son los inputs del formulario)
+    const inputSalarioB = document.getElementById("salarioB").value;
+    const inputRetencion = document.getElementById("retencion").value;
+    const inputNumPagas = document.getElementById("nPagas").value;
+
+    //Convertimos los valores del DOM
+    const salarioBrutoAnual = parseFloat(inputSalarioB);
+    const porcentajeRetencion = parseFloat(inputRetencion);
+    const numPagas = parseInt(inputNumPagas);
+
+    const salarioNetoAnual = salarioBrutoAnual * (1 - (porcentajeRetencion / 100))
+    
+    const salarioNetoMensual = (salarioNetoAnual / numPagas).toFixed(2);
+
+    document.writeln(`El salario neto mensual/por paga es: ${salarioNetoMensual} €`);
 }
+
+botonGenerar.addEventListener('click', calculoSalarioNeto);
