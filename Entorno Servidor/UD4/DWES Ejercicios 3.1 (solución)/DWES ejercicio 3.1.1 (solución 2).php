f6450394354961001f0296b6bc54f59496cea0e1
José Javier <jpasamaracon@gmail.com>
2025-12-06 17:37:41 +0100

    a

---
+++ Entorno Servidor/UD4/DWES Ejercicios 3.1 (solución)/DWES ejercicio 3.1.1 (solución 2).php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>Ejercicio 3.1.1</title>
+		<link rel="stylesheet" type="text/css" href="estilos.css">
+		<link rel="stylesheet" type="text/css" href="estilosimple.css">
+	</head>
+	<body>
+		<?php
+		// Conectar con el servidor de base de datos:
+		$conexion = mysqli_connect ("localhost", "jardinero", "jardinero")
+			or die ("No se puede conectar con el servidor"); // Si hay un error en la conexión, se termina de ejecutar el script PHP.
+		
+		// Seleccionar base de datos:
+		mysqli_select_db ($conexion, "jardineria")
+			or die ("No se puede seleccionar la BD");
+		
+		$consulta = mysqli_query ($conexion, "SELECT * from clientes")
+			or die ("Fallo en la consulta");
+		
+		echo '<table border=1>';
+		
+		// PARA SACAR LA CABECERA CON LOS NOMBRES DE LOS CAMPOS:
+		// Con funciones de mysqli: mysqli_fetch_field
+		// Con funciones de mysql se haría con: mysql_field_name
+		echo "<tr>";
+		while ($finfo = mysqli_fetch_field($consulta)) {
+				$nombrecampo = $finfo->name; // Se obtiene el nombre de cada campo.
+			echo "<th bgcolor='cyan'>",$nombrecampo,"</th>";
+		}
+		echo "</tr>";
+		
+		// PARA SACAR LOS DATOS DE CADA FILA DE CLIENTES:
+		$nfilas = mysqli_num_rows ($consulta);
+		for ($f=1; $f<=$nfilas; $f++) {
+			echo "<tr>";
+			//$fila = mysqli_fetch_row ($consulta);
+			$fila = mysqli_fetch_assoc ($consulta);
+			//$fila = mysqli_fetch_array ($consulta); // !OJO!, éste así lo devuelve y enseña las columnas duplicadas (flag MYSQL_BOTH por defecto).
+			foreach ($fila as $columna) {
+				if ($columna)
+					echo "<td style='background:orange'>$columna</td>";
+				else
+					echo "<td style='background:red'>$columna</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+		mysqli_close ($conexion);
+		?>
+	</body>
+</html>
---
+++ Entorno Servidor/UD4/DWES Ejercicios 3.1 (solución)/DWES ejercicio 3.1.4 (solución).php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>Ejercicio 3.1.4</title>
+		<link rel="stylesheet" type="text/css" href="estilos.css">
+		<link rel="stylesheet" type="text/css" href="estilosimple.css">
+	</head>
+	<body>
+		<header>
+			<h1>CONSULTA DE CLIENTES POR PAIS</h1>
+		</header>
+		<section>
+		<nav></nav>
+			<main>
+				<?php
+				// Conectar con el servidor de base de datos:
+				$conexion = mysqli_connect ("localhost", "jardinero", "jardinero")
+					or die ("No se puede conectar con el servidor");
+				
+				// Seleccionar base de datos:
+				mysqli_select_db ($conexion, "jardineria")
+					or die ("No se puede seleccionar la base de datos");
+				
+				if (isset($_REQUEST['enviar'])) { // "isset" determina si una variable está definida y no es "null".
+					$pais = $_REQUEST['Pais'];
+					// Enviar consulta:
+					$instruccion = "SELECT CodigoCliente, NombreCliente, NombreContacto, ApellidoContacto FROM clientes WHERE Pais='$pais' ORDER BY CodigoCliente";
+					$resconsulta = mysqli_query ($conexion, $instruccion)
+						or die ("Fallo en la consulta");
+					// Mostrar resultados de la consulta:
+					echo "<h1>LISTADO  DE CLIENTES DE --".$pais."-- EN BD JARDINERIA</h1><br/>";
+					echo "<table>";
+					echo "<tr> <th>CÓDIGO</th> <th>NOMBRE</th> <th>NOMBRE CONTACTO</th> <th>APELLIDO CONTACTO</th> </tr>";
+					while ($resultado = mysqli_fetch_row ($resconsulta)) {
+						echo "<tr>";
+						for ($i=0; $i<4; $i++) {
+							echo"<td>" .$resultado[$i]. "</td>";
+						}
+						echo "</tr>";
+					}
+					echo "</table>";
+					echo "<br/><a href='DWES ejercicio 3.1.4 (solución).php'>Realizar nueva consulta</a>";
+				} else {
+					echo "<h1>Consulta de clientes por pais</h1><br/>";
+					$instruccion = "SELECT DISTINCT Pais FROM clientes ORDER BY Pais";
+					$resconsulta = mysqli_query ($conexion, $instruccion)
+						or die ("Fallo en la consulta");
+					
+					print ("<form action='DWES ejercicio 3.1.4 (solución).php' method='GET'>");
+					print ("Elige País &nbsp");
+					print ("<select name='Pais'>");
+					$nfilas = mysqli_num_rows ($resconsulta);
+					if ($nfilas>0) {
+						for ($i=1; $i<=$nfilas; $i++) {
+							$resultado = mysqli_fetch_row ($resconsulta);
+							echo "<option>". $resultado[0]."</option>";
+						}
+					}
+					print ("</select>");
+					print ("<br/><br/><p><input type='submit' name='enviar' value='Enviar consulta' /></p>");
+					print ("</form>");
+				}
+				// Cerrar conexión:
+				mysqli_close ($conexion);
+				?>
+			</main>		
+		<aside></aside>
+		</section>
+		<footer></footer>
+	</body>
+</html>
